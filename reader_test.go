edf2aff3e99f18ba348af13f12a635a00ff67496
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-11-22 18:25:00 -0500

    minor test improvements

--- reader_test.go
+++ reader_test.go
@@ -15,22 +15,37 @@ const eg = `!OpenWayback-CDXJ 1.0
 (uk,co,bbc,)/images> 2015-09-03T13:27:52Z response {"frequency":725,"spread":1}
 `
 
-var parsed = []*Record{
-	&Record{"cnn.com/world", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeResponse, map[string]interface{}{"a": 0, "b": "b", "c": false}},
-	&Record{"rpms.ac.uk/", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeRequest, map[string]interface{}{"frequency": 241, "spread": 3}},
-	&Record{"bbc.co.uk/images", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeResponse, map[string]interface{}{"frequency": 725, "spread": 1}},
-}
+const eg2 = `!OpenWayback-CDXJ 1.0
+((com,reddit,)> 2015-09-03T13:27:52Z request {}`
 
 func TestReader(t *testing.T) {
-	records, err := NewReader(strings.NewReader(eg)).ReadAll()
-	if err != nil {
-		t.Error(err)
-		return
+	cases := []struct {
+		in     string
+		expect []*Record
+		err    string
+	}{
+		{eg, []*Record{
+			&Record{"cnn.com/world", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeResponse, map[string]interface{}{"a": 0, "b": "b", "c": false}},
+			&Record{"rpms.ac.uk/", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeRequest, map[string]interface{}{"frequency": 241, "spread": 3}},
+			&Record{"bbc.co.uk/images", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeResponse, map[string]interface{}{"frequency": 725, "spread": 1}},
+		}, ""},
+		{eg2, []*Record{
+			&Record{"reddit.com", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeRequest, map[string]interface{}{}},
+		}, ""},
 	}
 
-	if err := CompareRecordSlices(records, parsed); err != nil {
-		t.Error(err)
-		return
+	for i, c := range cases {
+		records, err := NewReader(strings.NewReader(c.in)).ReadAll()
+		if !(err == nil && c.err == "" || err != nil && err.Error() == c.err) {
+			t.Errorf("case %d error mismatch. expected: '%s', got: '%s'", i, c.err, err.Error())
+			continue
+		}
+
+		if err := CompareRecordSlices(c.expect, records); err != nil {
+			t.Errorf("case %d record slice mistmatch: %s", i, err.Error())
+			continue
+		}
+
 	}
 }
 
--- writer_test.go
+++ writer_test.go
@@ -3,13 +3,21 @@ package cdxj
 import (
 	"bytes"
 	"testing"
+	"time"
+
+	"github.com/datatogether/warc"
 )
 
 func TestWriter(t *testing.T) {
+	records := []*Record{
+		&Record{"cnn.com/world", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeResponse, map[string]interface{}{"a": 0, "b": "b", "c": false}},
+		&Record{"rpms.ac.uk/", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeRequest, map[string]interface{}{"frequency": 241, "spread": 3}},
+		&Record{"bbc.co.uk/images", time.Date(2015, time.September, 3, 13, 27, 52, 0, time.UTC), warc.RecordTypeResponse, map[string]interface{}{"frequency": 725, "spread": 1}},
+	}
 	buf := &bytes.Buffer{}
 
 	w := NewWriter(buf)
-	for i, rec := range parsed {
+	for i, rec := range records {
 		if err := w.Write(rec); err != nil {
 			t.Errorf("error writing record %d: %s", i, err.Error())
 			return
